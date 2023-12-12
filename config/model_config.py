134ae74eea9b127cdf7ce8e1a177f68312b33ebd
wiratana <andikawiratana@gmail.com>
2023-12-12 23:26:51 +0800

    remove unused approach of integration

--- config/model_config.py
+++ config/model_config.py
@@ -1,15 +1,9 @@
-from transformers import pipeline, T5Tokenizer, TFT5ForConditionalGeneration, DistilBertTokenizer, TFDistilBertModel
+from transformers import pipeline
 
 
 def summarization_model_init():
-    # tokenizer = T5Tokenizer.from_pretrained("arthd24/wanderwise_summary_1", legacy=False)
-    # model = TFT5ForConditionalGeneration.from_pretrained("arthd24/wanderwise_summary_1")
-    # return tokenizer, model
     return pipeline('summarization', model="arthd24/wanderwise_summary_1")
 
 
 def classification_model_init():
-    # tokenizer = DistilBertTokenizer.from_pretrained("arthd24/wanderwise_classification_1")
-    # model = TFDistilBertModel.from_pretrained("arthd24/wanderwise_classification_1")
-    # return tokenizer, model
     return pipeline("text-classification", model="arthd24/wanderwise_classification_1")
